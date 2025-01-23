c5f575c9a6e768229845a25b84939cea88aea2e0
Tobias Fischer <tobiasfischer17@gmail.com>
2025-01-22 22:14:13 -0500

    model downloading

--- text/models.py
+++ text/models.py
@@ -1,4 +1,8 @@
+from tinygrad import Device, nn
+from tinygrad.helpers import fetch
+
 from extra.models.llama import Transformer
+
 from dataclasses import dataclass
 from typing import Dict, Tuple
 
@@ -6,7 +10,10 @@ from typing import Dict, Tuple
 class ModelInst:
    params: Dict
    weights_url: str
+   weights_subdir: str
    num_weights: int
+   index_filename: str = "model.safetensors.index.json"
+   chunk_filename: str = "model-{i:05d}-of-{num_weights:05d}.safetensors"
 
 @dataclass
 class ModelArch:
@@ -20,14 +27,20 @@ MODELS: Dict[str,ModelArch] = {
          "32B": ModelInst(
             params={"dim":5120, "n_heads":40, "n_kv_heads":8, "n_layers":64, "norm_eps":1e-5, "rope_theta":1000000, "vocab_size":152064, "hidden_dim":27648},
             weights_url="https://huggingface.co/Qwen/QwQ-32B-Preview/resolve/main",
+            weights_subdir="qwq_32b_preview",
             num_weights=17,
          ),
       }
    )
 }
 
-def load_model(arch:ModelArch, inst:ModelInst):
-   pass
+def load_model(arch:ModelArch, inst:ModelInst, device_mem:Dict[str,int]):
+   index_filepath = fetch(f"{inst.weights_url}/{inst.index_filename}?download=true", "model.safetensors.index.json", subdir=inst.weights_subdir)
+   for i in range(1, inst.num_weights+1):
+      filename = inst.chunk_filename.format(i=i, num_weights=inst.num_weights)
+      fetch(f"{inst.weights_url}/{filename}?download=true", filename, subdir=inst.weights_subdir)
+   
+   model = Transformer(**inst.params, linear=nn.Linear)
 
 if __name__ == "__main__":
    import argparse
@@ -36,8 +49,13 @@ if __name__ == "__main__":
    for arch_name, arch_val in MODELS.items():
       for inst_name, inst_val in arch_val.instances.items():
          model_options[f"{arch_name}-{inst_name}"] = (arch_val,inst_val)
-   parser.add_argument('model', choices=list(model_options.keys()))
+   parser.add_argument('model', choices=list(model_options.keys()), help="Which model to load and test")
+   parser.add_argument('--gpus', type=int, default=6, help="Number of GPUs allowed to be used")
+   parser.add_argument('--vram-limit', type=int, default=18, help="Amount of VRAM in GB per GPU to load weights")
    args = parser.parse_args()
 
    arch, inst = model_options[args.model]
-   load_model(arch, inst)
+   device_mem: Dict[str,int] = {}
+   for i in range(args.gpus):
+      device_mem[f"{Device.DEFAULT}:{i}"] = args.vram_limit*1024*1024*1024
+   load_model(arch, inst, device_mem)
