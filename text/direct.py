dab27cf9688733871996ba149b222b24e66b6a83
Tobias Fischer <tobiasfischer17@gmail.com>
2025-02-10 20:18:58 -0500

    trimmed down direct

--- text/direct.py
+++ text/direct.py
@@ -65,71 +65,6 @@ def load(fn:str):
   else:
     return torch_load(fn)
 
-# **** quantized linears ****
-class Int8Linear:
-  def __init__(self, in_features, out_features, bias=False):
-    assert bias == False
-    self.weight = Tensor.ones(out_features, in_features, dtype=dtypes.int8)
-    self.scale = Tensor.ones(out_features, dtype=dtypes.half)
-
-  def __call__(self, x):
-    return x.dot(self.weight.cast(dtype=dtypes.half).T*self.scale)
-
-  @staticmethod
-  def quantize(tensors, device):
-    new_tensors = {}
-    for name,v in tensors.items():
-      if "feed_forward" in name or "attention.w" in name:
-        assert "weight" in name, name
-        scale = v.abs().max(axis=1) / 127.0
-        int8_weight = (v.T/scale).T.cast(dtype=dtypes.int8)
-        new_tensors[name] = int8_weight
-        new_tensors[name.replace('weight', 'scale')] = scale
-        if isinstance(device, tuple):
-          new_tensors[name].shard_(device, axis=-1)
-          new_tensors[name.replace('weight', 'scale')].shard_(device, axis=None)
-      else:
-        new_tensors[name] = v
-    return new_tensors
-
-def NF4Linear(block_size):
-  _CODE = [
-    -1.0, -0.6961928009986877, -0.5250730514526367, -0.39491748809814453, -0.28444138169288635, -0.18477343022823334, -0.09105003625154495, 0.0,
-    0.07958029955625534, 0.16093020141124725, 0.24611230194568634, 0.33791524171829224, 0.44070982933044434, 0.5626170039176941, 0.7229568362236023, 1.0,
-  ]
-  CODE = Tensor.stack(*[Tensor(c, dtype=dtypes.float16) for c in _CODE])
-  class _NF4Linear:
-    def __init__(self, in_features, out_features, bias=False):
-      assert not bias, "bias not supported"
-      self.in_features, self.out_features = in_features, out_features
-      self.weight = Tensor.empty(int(out_features * in_features / 2), dtype=dtypes.uint8)
-      self.scale = Tensor.empty(int(out_features * in_features / block_size), 1, dtype=dtypes.float16)
-
-    def __call__(self, x: Tensor) -> Tensor:
-      high_bits = self.weight
-      low_bits = (self.weight * 2 ** 4).contiguous()
-      unpacked = Tensor.stack(high_bits, low_bits, dim=-1).idiv(2 ** 4)
-      unscaled = CODE[unpacked].to(x.device).reshape(-1, block_size) * self.scale
-      return x.linear(unscaled.reshape(self.out_features, self.in_features).T)
-
-    @staticmethod
-    def quantize(state_dict: dict[str, Tensor], device) -> dict[str, Tensor]:
-      new_state_dict = {}
-      for k, v in state_dict.items():
-        if "feed_forward" in k or "attention.w" in k:
-          grouped = v.reshape(-1, block_size)
-          scale = (grouped.abs().max(axis=1, keepdim=True))
-          coded = ((grouped / scale).unsqueeze(-1) - CODE.to(v.device)).abs().argmin(axis=-1).cast(dtypes.uint8).flatten()
-          new_state_dict[k] = coded[::2] * 2 ** 4 + coded[1::2]
-          new_state_dict[k.replace(".weight", ".scale")] = scale.cast(dtypes.float16)
-          if isinstance(device, tuple):
-            new_state_dict[k].shard_(device, axis=-1)
-            new_state_dict[k.replace('weight', 'scale')].shard_(device, axis=None)
-        else:
-          new_state_dict[k] = v
-      return new_state_dict
-  return _NF4Linear
-
 MODEL_CFG = ModelConfig(dim=4096, hidden_dim=14336, n_layers=32, n_heads=32, n_kv_heads=8, norm_eps=1e-5, vocab_size=128256, rope_theta=500000.0, max_context=8192)
 MODEL_FILES = 1
 
@@ -167,12 +102,6 @@ def build_transformer(model_path:Path, cfg:ModelConfig, files:int, device=None):
     load_state_dict(model, weights, strict=False, consume=True)
   return model
 
-# default settings
-TEMPERATURE = 0.95
-TOP_K = 0
-TOP_P = 0.0
-ALPHA_F = 0.0
-ALPHA_P = 0.0
 SAMPLER = TokenSampler(
    temperature=0.95,
    top_k=0,
@@ -180,26 +109,6 @@ SAMPLER = TokenSampler(
    alpha_f=0.0,
    alpha_p=0.0,
 )
-
-last_seen_toks = []
-def prefill(model, toks, start_pos=0):
-  global last_seen_toks
-
-  # we can skip part of the prompt if it is the same as last and start_pos=0
-  if start_pos == 0:
-    for i, (a, b) in enumerate(zip(toks, last_seen_toks)):
-      if a != b: break
-    else: i = min(len(toks), len(last_seen_toks))
-    start_pos += i
-    last_seen_toks = toks
-    toks = toks[i:]
-
-  # prefill the model
-  for tok in tqdm(toks):
-    GlobalCounters.reset()
-    model(Tensor([[tok]], device=device), start_pos, TEMPERATURE, TOP_K, TOP_P, ALPHA_F, ALPHA_P).realize()
-    start_pos += 1
-  return start_pos
 
 if __name__ == "__main__":
   Tensor.no_grad = True
